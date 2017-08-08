65de356936a89c00232cd938543452166d5e43b3
K.Emre Akkaya <30558101+KEmreAkkaya@users.noreply.github.com>
2017-08-08 11:29:47 +0300

    Create RSA.cpp

---
+++ RSA.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "rsa.h"
+
+
+
+
+
+using namespace std;
+
+int main()
+{   string x;
+	RSA bilgi;
+	
+	bilgi.pAyarla(13);
+	bilgi.qAyarla(23);
+	bilgi.nAyarla();
+    bilgi.esec(35);
+	bilgi.dOlustur();
+
+	
+	
+	cout << "String Giriniz:";
+	cin >> x;
+
+    bilgi.ciphermetin(x);
+	
+	bilgi.deciphermetin();
+	
+	system("pause");
+	return 0;
+}
